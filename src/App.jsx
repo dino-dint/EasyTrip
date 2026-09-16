e0722d8bb600ac814ab74d55f707736d2710e5dd
dino-dint <rimurusimping@gmail.com>
2026-09-16 16:45:21 +0700

    wrap approutes inside authprovider

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
-import React from 'react'
-import AppRoutes from './routes/AppRoutes'
-// import { AuthProvider } from './context/AuthContext'
+import AppRoutes from "./routes/AppRoutes";
+import { AuthProvider } from "./context/AuthContext";
 // import { BookingProvider } from './context/BookingContext'
 // import { WishProvider } from './context/WIshlistContext'
 
-
 function App() {
   return (
-    // <AuthProvider>
-    //   <BookingProvider>
-    //     <WishProvider>
-          <AppRoutes/>
-    //     </WishProvider>
-    //   </BookingProvider>
-    // </AuthProvider>
-  )
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
