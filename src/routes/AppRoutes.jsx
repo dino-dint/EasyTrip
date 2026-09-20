20176af036fff7457b83fab02d3ba05957c50465
dino-dint <rimurusimping@gmail.com>
2026-09-20 19:24:57 +0700

    add home route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,14 +5,16 @@ import Register from '../pages/Auth/Register'
 import ForgotPassword from '../pages/Auth/ForgotPassword'
 import ComingSoon from '../pages/ComingSoon/ComingSoon'
 import Home from '../pages/Home/Home'
-
+import DestinationPage from '../pages/Destination/DestinationPage'
+import SearchResults from '../pages/Find/SearchResults'
 
 function AppRoutes() {
   return (
     <Routes>
       <Route element={<MainLayout/>}>
         <Route path='/' element={<Home/>} />
-        <Route path='/destination' element={<ComingSoon title="Destination"/>}/>
+        <Route path='/destination/:country' element={<DestinationPage/>}/>
+        <Route path='/search' element={<SearchResults/>}/>
         <Route path='/accommodation' element={<ComingSoon title="Hotels & Resorts"/>}/>
         <Route path='/flight' element={<ComingSoon title="Flights"/>}/>
         <Route path='/contact' element={<ComingSoon title="Contact"/>}/>
