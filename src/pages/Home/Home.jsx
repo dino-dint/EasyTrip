f0322880d3b5911954d2b81a13b1ed9ee2991fe2
dino-dint <rimurusimping@gmail.com>
2026-09-20 19:23:40 +0700

    complete homepage0

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,18 @@
 import Navbar from '../../components/layouts/Navbar'
+import Features from './Features'
 import Hero from './Hero'
+import Locations from './Locations'
+import PopularDestination from './PopularDestination'
+import Testimonials from './Testimonials'
 
 function Home() {
   return (
     <div className='relative'>
         <Hero/>
+        <Features/>
+        <Locations/>
+        <PopularDestination/>
+        <Testimonials/>
     </div>
   )
 }
