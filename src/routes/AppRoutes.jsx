5d9bd299222c196309d7e410afd8949b34cf0ddc
dino-dint <rimurusimping@gmail.com>
2026-09-20 23:19:30 +0700

    add some new routes to the mainroutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,39 +1,42 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import MainLayout from '../components/layouts/MainLayout'
-import Login from '../pages/Auth/Login'
-import Register from '../pages/Auth/Register'
-import ForgotPassword from '../pages/Auth/ForgotPassword'
-import ComingSoon from '../pages/ComingSoon/ComingSoon'
-import Home from '../pages/Home/Home'
-import DestinationPage from '../pages/Destination/DestinationPage'
-import SearchResults from '../pages/Find/SearchResults'
+import { Routes, Route, Navigate } from "react-router-dom";
+import MainLayout from "../components/layouts/MainLayout";
+import Login from "../pages/Auth/Login";
+import Register from "../pages/Auth/Register";
+import ForgotPassword from "../pages/Auth/ForgotPassword";
+import ComingSoon from "../pages/ComingSoon/ComingSoon";
+import Home from "../pages/Home/Home";
+import DestinationPage from "../pages/Destination/DestinationPage";
+import SearchResults from "../pages/Find/SearchResults";
+import Contact from "../pages/Contact/Contact";
+import Wishlist from "../pages/Wishlist/Wishlist";
+import MyTrip from "../pages/MyTrips/MyTrips";
+import Profile from "../pages/Profile/Profile";
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route element={<MainLayout/>}>
-        <Route path='/' element={<Home/>} />
-        <Route path='/destination/:country' element={<DestinationPage/>}/>
-        <Route path='/search' element={<SearchResults/>}/>
-        <Route path='/accommodation' element={<ComingSoon title="Hotels & Resorts"/>}/>
-        <Route path='/flight' element={<ComingSoon title="Flights"/>}/>
-        <Route path='/contact' element={<ComingSoon title="Contact"/>}/>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/destination/:country" element={<DestinationPage />} />
+        <Route path="/search" element={<SearchResults />} />
+        <Route
+          path="/accommodation"
+          element={<ComingSoon title="Hotels & Resorts" />}
+        />
+        <Route path="/flight" element={<ComingSoon title="Flights" />} />
+        <Route path="/wishlist" element={<Wishlist/>}/>
+        <Route path="/my-trip" element={<MyTrip/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/contact" element={<Contact />} />
       </Route>
-      
 
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/forgot" element={<ForgotPassword />} />
 
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/register' element={<Register/>}/>
-      <Route path='/forgot' element={<ForgotPassword/>}/>
-
-       {/* Optional fallback */}
-      <Route
-        path="*"
-        element={<Navigate to="/" replace />}
-      />
+      {/* Optional fallback */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
-  )
+  );
 }
-export default AppRoutes
-
-
+export default AppRoutes;
