e2e93bef44f3e8c1f6eb91d86b11b5a0e622bb97
dino-dint <rimurusimping@gmail.com>
2026-09-21 14:26:05 +0700

    add routes to new page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,28 +5,30 @@ import Register from "../pages/Auth/Register";
 import ForgotPassword from "../pages/Auth/ForgotPassword";
 import ComingSoon from "../pages/ComingSoon/ComingSoon";
 import Home from "../pages/Home/Home";
-import DestinationPage from "../pages/Destination/DestinationPage";
+import Destination from "../pages/Destination/Destination";
 import SearchResults from "../pages/Find/SearchResults";
 import Contact from "../pages/Contact/Contact";
 import Wishlist from "../pages/Wishlist/Wishlist";
 import MyTrip from "../pages/MyTrips/MyTrips";
 import Profile from "../pages/Profile/Profile";
+import DestinationDetails from "../pages/Destination/DestinationDetails";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/destination/:country" element={<DestinationPage />} />
+        <Route path="destination" element={<Destination/>} />
+        <Route path="destination/:slug" element={<DestinationDetails/>} />
         <Route path="/search" element={<SearchResults />} />
         <Route
           path="/accommodation"
           element={<ComingSoon title="Hotels & Resorts" />}
         />
         <Route path="/flight" element={<ComingSoon title="Flights" />} />
-        <Route path="/wishlist" element={<Wishlist/>}/>
-        <Route path="/my-trip" element={<MyTrip/>}/>
-        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/my-trip" element={<MyTrip />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/contact" element={<Contact />} />
       </Route>
 
