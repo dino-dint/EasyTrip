2e2c9b9f8c09d6ae563f4e0b2907a4933ed2a51e
Panha_will <phycomob50@gmail.com>
2026-09-21 10:58:32 +0700

    hotel update

--- src/pages/Hotels/HotelDetails.jsx
+++ src/pages/Hotels/HotelDetails.jsx
@@ -149,8 +149,6 @@ function HotelDetailsInner({ hotel }) {
 
       {/* Body */}
       <main className="relative mx-auto max-w-7xl overflow-x-clip px-4 py-12 sm:px-6 lg:px-8">
-        <div className="pointer-events-none absolute -left-20 top-20 h-64 w-64 rounded-full bg-sky-400/20 blur-3xl" />
-
         <div className="grid gap-10 lg:grid-cols-[1.55fr_1fr] lg:items-start">
           {/* Left column */}
           <div className="space-y-12">
